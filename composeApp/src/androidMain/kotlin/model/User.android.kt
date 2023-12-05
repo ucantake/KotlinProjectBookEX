55d9cf09b593aa9f27cf3d499ad0dc1723104f63
ucantake <andrei.vorotelenko@yandex.kz>
2023-12-05 08:56:14 +0600

    add model business object, use pattern expect-actual; and add BookExchange.sol - smart-contranct in the language solidity

---
+++ composeApp/src/androidMain/kotlin/model/User.android.kt
@@ -0,0 +1,11 @@
+package model
+
+actual interface User {
+    actual val firstName: String
+    actual val lastName: String
+    actual val username: String
+    actual val password: String
+    actual val balance: Any
+    actual val cryptoWalletLink: String
+    actual val books: List<Any>
+}
---
+++ composeApp/src/desktopMain/kotlin/model/Book.desktop.kt
@@ -0,0 +1,14 @@
+package model
+
+actual interface Book {
+    actual val id: String
+    actual val title: String
+    actual val author: String
+    actual val genre: String
+    actual val description: String
+    actual val publicationYear: Int
+    actual val isbn: String
+    actual val udk: String
+    actual val bbk: String
+    actual val exchangePrice: Any
+}
